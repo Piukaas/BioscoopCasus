a771c68073b6a36038ab2988cc39aba64bc1cfeb
pjotrvp <74866171+pjotrvp@users.noreply.github.com>
2024-02-01 16:33:45 +0100

    Testing

--- src/test/java/cinema/OrderTests.java
+++ src/test/java/cinema/OrderTests.java
@@ -14,8 +14,13 @@ class OrderTests {
     private LocalDateTime saturdayDateTime;
     private MovieScreening movieScreening;
     private MovieScreening weekendMovieScreening;
-    private MovieTicket regularMovieTicket;
-    private MovieTicket premiumMovieTicket;
+    private MovieTicket regularMovieTicketWeekDay;
+    private MovieTicket premiumMovieTicketWeekDay;
+    private MovieTicket regularMovieTicketWeekend;
+    private MovieTicket premiumMovieTicketWeekend;
+    private Order studentOrder;
+    private Order order;
+
     @BeforeEach
     void setUp() {
         movie = new Movie("The Matrix");
@@ -26,28 +31,65 @@ class OrderTests {
         // Saturday
         saturdayDateTime = LocalDateTime.of(2024, 2, 3, 19, 0);
         weekendMovieScreening = new MovieScreening(otherMovie, saturdayDateTime, 10.0);
-        regularMovieTicket = new MovieTicket(movieScreening, 1, 1, false);
-        premiumMovieTicket = new MovieTicket(movieScreening, 1, 1, true);
+        regularMovieTicketWeekDay = new MovieTicket(movieScreening, 1, 1, false);
+        premiumMovieTicketWeekDay = new MovieTicket(movieScreening, 1, 1, true);
+        regularMovieTicketWeekend = new MovieTicket(weekendMovieScreening, 1, 1, false);
+        premiumMovieTicketWeekend = new MovieTicket(weekendMovieScreening, 1, 1, true);
+        studentOrder = new Order(2, true);
+        order = new Order(1, false);
     }
 
     @Test
     void testCalculatePrice_StudentOrderThreeTickets() {
-        // Create a student order
-        Order studentOrder = new Order(2, true);
-        studentOrder.addSeatReservation(regularMovieTicket);
-        studentOrder.addSeatReservation(regularMovieTicket);
-        studentOrder.addSeatReservation(regularMovieTicket);
+        studentOrder.addSeatReservation(regularMovieTicketWeekDay);
+        studentOrder.addSeatReservation(regularMovieTicketWeekDay);
+        studentOrder.addSeatReservation(regularMovieTicketWeekDay);
         assertEquals(20.0, studentOrder.calculatePrice());
-    }    
+    }
 
     @Test
     void testCalculatePrice_StudentOrderFourTickets() {
-        Order studentOrder = new Order(2, true);
-        studentOrder.addSeatReservation(regularMovieTicket);
-        studentOrder.addSeatReservation(regularMovieTicket);
-        studentOrder.addSeatReservation(regularMovieTicket);
-        studentOrder.addSeatReservation(regularMovieTicket);
+        studentOrder.addSeatReservation(regularMovieTicketWeekDay);
+        studentOrder.addSeatReservation(regularMovieTicketWeekDay);
+        studentOrder.addSeatReservation(regularMovieTicketWeekDay);
+        studentOrder.addSeatReservation(regularMovieTicketWeekDay);
         assertEquals(20.0, studentOrder.calculatePrice());
+    }
+
+    @Test
+    void testCalculatePrice_StudentOrderTwoPremiumTickets() {
+        studentOrder.addSeatReservation(premiumMovieTicketWeekDay);
+        studentOrder.addSeatReservation(premiumMovieTicketWeekDay);
+        assertEquals(12.0, studentOrder.calculatePrice());
+    }
+
+    @Test
+    void testCalculatePrice_StudentOrderThreePremiumTickets() {
+        studentOrder.addSeatReservation(premiumMovieTicketWeekDay);
+        studentOrder.addSeatReservation(premiumMovieTicketWeekDay);
+        studentOrder.addSeatReservation(premiumMovieTicketWeekDay);
+        assertEquals(24.0, studentOrder.calculatePrice());
+    }
+
+    @Test
+    void testCalculatePrice_regularOrderOnWeekDaySecondTicketFree() {
+        order.addSeatReservation(regularMovieTicketWeekDay);
+        order.addSeatReservation(regularMovieTicketWeekDay);
+        assertEquals(10.0, order.calculatePrice());
+    }
+
+    @Test
+    void testCalculatePrice_regularOrderOnWeekendIsFullPrice() {
+        order.addSeatReservation(regularMovieTicketWeekend);
+        order.addSeatReservation(regularMovieTicketWeekend);
+        assertEquals(20.0, order.calculatePrice());
+    }
+
+    @Test
+    void testCalculatePrice_studentOrderOnWeekendSecondTicketFree() {
+        studentOrder.addSeatReservation(regularMovieTicketWeekend);
+        studentOrder.addSeatReservation(regularMovieTicketWeekend);
+        assertEquals(10.0, studentOrder.calculatePrice());
     }
 
 }
